92480c5bae47a6b58083f329be01346c0aab47d7
Frank Oh <frankyoh@tmon.co.kr>
2019-01-04 10:54:04 +0900

    chap8 dao-support example added

---
+++ chap8-dao-support/src/main/java/org/mybatis/persistence/ShopDao.java
@@ -0,0 +1,17 @@
+package org.mybatis.persistence;
+
+import org.mybatis.domain.Shop;
+
+import java.util.List;
+
+public interface ShopDao {
+	public List<Shop> list(Shop shop);
+
+	public void insert(Shop shop);
+
+	public Shop select(String shopNo);
+
+	public void update(Shop shop);
+
+	public void delete(String shopNo);
+}
